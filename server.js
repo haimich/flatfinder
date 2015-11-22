d3646a69cf19137873949d53f741ac72c6864f90
Michael Müller <haimich@gmx.net>
2015-11-22 20:30:42 +0100

    testint with heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(cool());
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
 
