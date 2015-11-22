f31ddd9ff603d410e64e9dcbb1bdd72ac4855a0d
Michael Müller <haimich@gmx.net>
2015-11-22 20:42:25 +0100

    added postgres

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function (req, res) {
   res.send(cool());
 });
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
