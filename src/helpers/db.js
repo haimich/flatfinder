38bdd4e0f4c6b3900a8fbade271806354426f9dc
Michael Müller <haimich@gmx.net>
2015-12-17 19:00:26 +0100

    testing

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -7,24 +7,15 @@ let instance = null;
 module.exports.getInstance = () => {
   if (instance === null) {
     
-    try {
-      instance = knexDb({
-        client: 'pg',
-        debug: true,
-        connection: {
-          connection: process.env.DATABASE_URL,
-          database: 'd1ou8ff1cn9r17',
-          timezone: 'UTC',
-        },
-        pool: {
-          min: 0,
-          max: 1,
-        }
-      });
-    } catch (err) {
-      console.log('ERROR', err.stack);
-      throw err;
-    }
+    instance = knexDb({
+      client: 'pg',
+      debug: true,
+      connection: process.env.DATABASE_URL,
+      pool: {
+        min: 0,
+        max: 1,
+      }
+    });
   }
   
   return instance;
