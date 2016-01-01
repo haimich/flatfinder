d3ba967ee2ffa8ff1af7355a14e3a3855befcb3c
Michael Müller <michael.mueller@1und1.de>
2016-01-01 21:45:25 +0100

    added adapter

--- sql/migrations/20160101213940_companies_v7.js
+++ sql/migrations/20160101213940_companies_v7.js
@@ -1,12 +1,14 @@
 
 exports.up = (knex, Promise) => {
   return Promise.all([
-    knex('companies').insert({ id: 'besserwohnen', name: 'Besser Wohnen in Karlsruhe' })
+    knex('companies').insert({ id: 'besserwohnen', name: 'Besser Wohnen in Karlsruhe' }),
+    knex('companies').insert({ id: 'haitz', name: 'Haitz' })
   ]);
 };
 
 exports.down = (knex, Promise) => {
   return Promise.all([
-    knex('companies').where('id', 'besserwohnen').delete()
+    knex('companies').where('id', 'besserwohnen').delete(),
+    knex('companies').where('id', 'haitz').delete()    
   ]);
 };
---
+++ src/adapters/haitz.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let request = require('request-promise');
+let cheerio = require('cheerio');
+let Flat = require('../models/Flat');
+
+const COMPANY_ID = 'haitz';
+const URL = 'http://www.haitz-gmbh.de/';
+
+module.exports.scrape = () => {
+  console.log('Scraping', COMPANY_ID);
+  return request(URL + 'index.php?id=7')
+    .then(response => {
+      let flats = [];
+      let $ = cheerio.load(response);
+      
+      $('#content .csc-textpicHeader').each((i, el) => {
+        let title = $(el).text().trim();
+        let flatUrl = $(el).parent().find('a').attr('href');
+        
+        let flat = new Flat(COMPANY_ID, title, URL + flatUrl);
+        flats.push(flat);
+      });
+      console.log(flats);
+      return flats;
+    }); 
+}
