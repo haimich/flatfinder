7f74d7d31727c7ec2bc6dd2af229115c97778a66
Michael Müller <michael.mueller@1und1.de>
2016-01-15 17:05:15 +0100

    simple

--- src/adapters/leitgieb.js
+++ src/adapters/leitgieb.js
@@ -1,29 +1,17 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'leitgieb';
-const URL = 'http://www.leitgieb-immobilien.de/html/immobilien.html';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request({
-    uri: URL,
-    encoding: 'binary'
-  })
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('p span b u').each((i, el) => {
-        let title = $(el).text().trim();
-        
-        let flat = new Flat(COMPANY_ID, title, URL);
-        flats.push(flat);
-      });
-      
-      return flats;
-    }); 
+  let adapter = new SimpleAdapter(
+    COMPANY_ID,
+    'http://www.leitgieb-immobilien.de/html/immobilien.html',
+    'p span b u', {
+      encoding: 'binary'
+    }
+  );
+  
+  return adapter.scrape();
 }
