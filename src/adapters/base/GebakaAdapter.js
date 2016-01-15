f18ebdcb7267a0c4fcc9812e913ee117f2fb759c
Michael Müller <haimich@gmx.net>
2016-01-15 23:53:21 +0100

    gebaka

---
+++ src/adapters/base/GebakaAdapter.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let request = require('request-promise');
+let cheerio = require('cheerio');
+let Adapter = require('./Adapter');
+let Flat = require('../../models/Flat');
+let UA = require('../../models/UserAgent');
+
+class GebakaAdapter extends Adapter {
+  
+  constructor(companyId, baseUrl) {
+    super(companyId, baseUrl, '#index2content > div');
+    
+    this.urlSuffix = '';
+    this.getUrlFromElement = null;
+    this.useAbsoluteUrls = false;
+    this.encoding = 'utf8';
+  }
+  
+  scrape() {
+    console.log('Scraping', this.companyId);
+    
+    return request({
+      uri: this.baseUrl + this.urlSuffix,
+      encoding: this.encoding,
+      headers: {
+        'User-Agent': UA.FIREFOX
+      }
+    }).then(response => {
+        let flats = [];
+        let $ = cheerio.load(response);
+        
+        $(this.searchString).each((i, el) => {
+          if ($(el).css('width') !== '400px') {
+            return;
+          }
+          
+          let title = $(el).find('.ueberschrift').text().trim();
+          
+          if (title === '' || this.isBlacklisted(this.titleBlacklist, title)) {
+            return;
+          }
+          
+          let flat = new Flat(this.companyId, title, this.baseUrl);
+          flats.push(flat);
+        });
+        
+        console.log(flats, this.companyId);
+        return flats;
+      });
+  }
+  
+}
+
+module.exports = GebakaAdapter;
--- src/adapters/gebaka.js
+++ src/adapters/gebaka.js
@@ -1,28 +1,14 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let GebakaAdapter = require('./base/GebakaAdapter');
 
 const COMPANY_ID = 'gebaka';
-const URL = 'http://www.gebaka.de/index2.php?site=objekteaktuell&umenue=1&kategorie=Kaufobjekte';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request(URL)
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('#index2content > div').each((i, el) => {
-        if ($(el).css('width') === '400px') {
-          let text = $(el).find('.ueberschrift').text().trim();
-          
-          let flat = new Flat(COMPANY_ID, text, URL);
-          flats.push(flat);
-        }
-      });
-      
-      return flats;
-    }); 
+  let adapter = new GebakaAdapter(
+    COMPANY_ID,
+    'http://www.gebaka.de/index2.php?site=objekteaktuell&umenue=1&kategorie=Kaufobjekte'
+  );
+  
+  return adapter.scrape();
 }
