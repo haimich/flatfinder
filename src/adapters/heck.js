f00c4e55c4dcaf1bf659010b8c0403a5e108addd
Michael Müller <michael.mueller@1und1.de>
2016-01-15 17:01:13 +0100

    simple

--- src/adapters/heck.js
+++ src/adapters/heck.js
@@ -1,27 +1,17 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'heck';
-const URL = 'http://www.heck-immobilien.de';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request(URL + '/immobilien-angebote/eigentumswohnungen/')
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('#content .realestate .title a').each((i, el) => {
-        let title = $(el).text().trim();
-        let flatUrl = $(el).attr('href');
-        
-        let flat = new Flat(COMPANY_ID, title, URL + flatUrl);
-        flats.push(flat);
-      });
-      
-      return flats;
-    }); 
+  let adapter = new SimpleAdapter(
+    COMPANY_ID,
+    'http://www.heck-immobilien.de',
+    '#content .realestate .title a', {
+      urlSuffix: '/immobilien-angebote/eigentumswohnungen/'
+    }
+  );
+  
+  return adapter.scrape();
 }
