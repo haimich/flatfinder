9b02e7884a5589fd9f2f97e6a842253642c5f221
Michael Müller <michael.mueller@1und1.de>
2016-01-15 17:41:35 +0100

    simple

--- src/adapters/volkswohnung.js
+++ src/adapters/volkswohnung.js
@@ -1,26 +1,15 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'volkswohnung';
-const URL = 'https://www.volkswohnung.com/angebote/kaufen/';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request(URL)
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('.tx-offers').each((i, el) => {
-        let title = $(el).text().trim();
-        
-        let flat = new Flat(COMPANY_ID, title, URL);
-        flats.push(flat);
-      });
-      
-      return flats;
-    }); 
+  let adapter = new SimpleAdapter(
+    COMPANY_ID,
+    'https://www.volkswohnung.com/angebote/kaufen/',
+    '.tx-offers'
+  );
+  
+  return adapter.scrape();
 }
