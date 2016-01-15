010c2b62914fa4958efa5795e937b79eabca42f6
Michael Müller <michael.mueller@1und1.de>
2016-01-15 17:40:25 +0100

    simple

--- src/adapters/throm.js
+++ src/adapters/throm.js
@@ -1,26 +1,15 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'throm';
-const URL = 'http://throm.de/angebote-verkauf.php';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request(URL)
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('.angebot-txt').each((i, el) => {
-        let text = $(el).find('.newstitle').text().trim();
-        
-        let flat = new Flat(COMPANY_ID, text, URL);
-        flats.push(flat);
-      });
-      
-      return flats;
-    }); 
+  let adapter = new SimpleAdapter(
+    COMPANY_ID,
+    'http://throm.de/angebote-verkauf.php',
+    '.angebot-txt .newstitle'
+  );
+  
+  return adapter.scrape();
 }
