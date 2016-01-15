0f9b32adf7cd1cd6984b664d1248990f61df3bd6
Michael Müller <michael.mueller@1und1.de>
2016-01-15 17:35:54 +0100

    simple

--- src/adapters/seegerrusswurm.js
+++ src/adapters/seegerrusswurm.js
@@ -1,27 +1,15 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'seegerrusswurm';
-const URL = 'http://www.seeger-russwurm.de/immobilienfinder/wohnimmobilien.php?immobilienart=wohnung&finanzierung=kauf&preis_bis=%E2%88%9E&preis_von=180.000&groesse_bis=%E2%88%9E&groesse_von=100&zimmer_bis=%E2%88%9E&zimmer_von=3';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request(URL)
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('#content .objekt_headline a').each((i, el) => {
-        let title = $(el).text().trim();
-        let flatUrl = $(el).attr('href');
-        
-        let flat = new Flat(COMPANY_ID, title, flatUrl);
-        flats.push(flat);
-      });
-        
-      return flats;
-    }); 
+  let adapter = new SimpleAdapter(
+    COMPANY_ID,
+    'http://www.seeger-russwurm.de/immobilienfinder/wohnimmobilien.php?immobilienart=wohnung&finanzierung=kauf&preis_bis=%E2%88%9E&preis_von=180.000&groesse_bis=%E2%88%9E&groesse_von=100&zimmer_bis=%E2%88%9E&zimmer_von=3',
+    '#content .objekt_headline a'
+  );
+  
+  return adapter.scrape();
 }
