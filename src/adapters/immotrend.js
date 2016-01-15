60cd704c994a17e0d9fb5f36100c7dd4739bb756
Michael Müller <michael.mueller@1und1.de>
2016-01-15 17:03:00 +0100

    simple

--- src/adapters/immotrend.js
+++ src/adapters/immotrend.js
@@ -1,30 +1,17 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'immotrend';
-const URL = 'http://www.immotrend.com/';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request({
-    uri: URL,
-    encoding: 'binary'
-  })
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('#menu .first .third li a').each((i, el) => {
-        let title = $(el).text().trim();
-        let flatUrl = $(el).attr('href');
-        
-        let flat = new Flat(COMPANY_ID, title, URL + flatUrl);
-        flats.push(flat);
-      });
-      
-      return flats;
-    }); 
+  let adapter = new SimpleAdapter(
+    COMPANY_ID,
+    'http://www.immotrend.com/',
+    '#menu .first .third li a', {
+      encoding: 'binary'
+    }
+  );
+  
+  return adapter.scrape();
 }
