d3238a3af65e3ae60d4475d68e015a989bc096ba
Michael Müller <michael.mueller@1und1.de>
2016-01-15 17:42:55 +0100

    simple

--- src/adapters/wipfler.js
+++ src/adapters/wipfler.js
@@ -1,27 +1,17 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'wipfler';
-const URL = 'http://www.wipfler-gmbh.de/immobilien/neubauprojekte/';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request(URL)
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('#mainNav2 #mainNav3 a').each((i, el) => {
-        let title = $(el).text().trim();
-        let flatUrl = $(el).attr('href');
-        
-        let flat = new Flat(COMPANY_ID, title, flatUrl);
-        flats.push(flat);
-      });
-      
-      return flats;
-    }); 
+  let adapter = new SimpleAdapter(
+    COMPANY_ID,
+    'http://www.wipfler-gmbh.de/immobilien/neubauprojekte/',
+    '#mainNav2 #mainNav3 a', {
+      useAbsoluteUrls: true
+    }
+  );
+  
+  return adapter.scrape();
 }
