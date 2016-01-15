8174dd45112025c63d554b97781145a857660dae
Michael Müller <michael.mueller@1und1.de>
2016-01-15 17:07:01 +0100

    simple

--- src/adapters/neubaukompass.js
+++ src/adapters/neubaukompass.js
@@ -1,27 +1,17 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'neubaukompass';
-const URL = 'http://rhein-neckar.neubaukompass.de';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request(URL + '/Karlsruhe/Immobilien-in-Karlsruhe.html')
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('.itm-neubau-projekte h2 a').each((i, el) => {
-        let title = $(el).text().trim();
-        let flatUrl = $(el).attr('href');
-        
-        let flat = new Flat(COMPANY_ID, title, URL + flatUrl);
-        flats.push(flat);
-      });
-      
-      return flats;
-    }); 
+  let adapter = new SimpleAdapter(
+    COMPANY_ID,
+    'http://rhein-neckar.neubaukompass.de',
+    '.itm-neubau-projekte h2 a', {
+      urlSuffix: '/Karlsruhe/Immobilien-in-Karlsruhe.html'
+    }
+  );
+  
+  return adapter.scrape();
 }
