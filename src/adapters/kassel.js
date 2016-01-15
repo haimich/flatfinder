c6f40a9caa81642fafd2a35c3a1b0fd284ead1b7
Michael Müller <haimich@gmx.net>
2016-01-15 23:27:16 +0100

    kassel

--- src/adapters/kassel.js
+++ src/adapters/kassel.js
@@ -3,12 +3,28 @@
 let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'kassel';
+const URL_REGEX = /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/ig; // might match too much!
+
+let baseUrl = 'https://www.facebook.com/ImmobilienKassel?_fb_noscript=1';
 
 module.exports.scrape = () => {
   let adapter = new SimpleAdapter(
     COMPANY_ID,
-    'https://www.facebook.com/ImmobilienKassel?_fb_noscript=1',
-    '.userContent'
+    baseUrl,
+    '.userContent', {
+      useAbsoluteUrls: true,
+      getUrlFromElement: ($el) => {
+        let title = $el.text().trim();
+        
+        let urls = title.match(URL_REGEX);
+        
+        if (urls && urls.length >= 1) {
+          return urls[0];
+        } else {
+          return baseUrl;
+        }
+      }
+    }
   );
   
   return adapter.scrape();
