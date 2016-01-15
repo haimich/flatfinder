2bc00875fb34dcbbeccb78fad3b58c24def078a8
Michael Müller <michael.mueller@1und1.de>
2016-01-15 18:29:08 +0100

    simple

--- src/adapters/haitz.js
+++ src/adapters/haitz.js
@@ -1,27 +1,20 @@
 'use strict';
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let Flat = require('../models/Flat');
+let SimpleAdapter = require('./base/SimpleAdapter');
 
 const COMPANY_ID = 'haitz';
-const URL = 'http://www.haitz-gmbh.de/';
 
 module.exports.scrape = () => {
-  console.log('Scraping', COMPANY_ID);
-  return request(URL + 'index.php?id=7')
-    .then(response => {
-      let flats = [];
-      let $ = cheerio.load(response);
-      
-      $('#content .csc-textpicHeader').each((i, el) => {
-        let title = $(el).text().trim();
-        let flatUrl = $(el).parent().find('a').attr('href');
-        
-        let flat = new Flat(COMPANY_ID, title, URL + flatUrl);
-        flats.push(flat);
-      });
-      
-      return flats;
-    }); 
+  let adapter = new SimpleAdapter(
+    COMPANY_ID,
+    'http://www.haitz-gmbh.de/',
+    '#content .csc-textpicHeader', {
+      urlSuffix: 'index.php?id=7',
+      getUrlFromElement: ($el) => {
+        return $el.parent().find('a').attr('href');
+      }
+    }
+  );
+  
+  return adapter.scrape();
 }
