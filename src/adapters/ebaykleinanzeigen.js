bb61a2150daf711a74a83e4661462c65a5baf136
Michael Müller <haimich@gmx.net>
2016-01-16 22:52:16 +0100

    fix for redirect

--- src/adapters/ebaykleinanzeigen.js
+++ src/adapters/ebaykleinanzeigen.js
@@ -5,7 +5,7 @@ let SimpleAdapter = require('./base/SimpleAdapter');
 module.exports.scrape = () => {
   let adapter = new SimpleAdapter(
     'ebaykleinanzeigen', 
-    'https://www.ebay-kleinanzeigen.de',
+    'http://www.ebay-kleinanzeigen.de',
     '#srchrslt-content article h2 a', {
       urlSuffix: '/s-wohnung-kaufen/karlsruhe/anzeige:angebote/preis:280000:500000/c196l9186+wohnung_kaufen.qm_i:100,160+wohnung_kaufen.zimmer_i:3,'
     }
